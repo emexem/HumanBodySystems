82faecaa37f2639e73c571e79309380882caaf90
emexem <mxmalik@gmail.com>
2014-04-21 17:00:54 -0400

    Final

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+	$('.navLink').click(function(e)
+	{ 
+		navClick(e) 
+	});
+	$('.dot').click(function(e)
+	{ 
+		dotClick(e) 
+	});
+	function navClick(e)
+	{
+		$('#blurbs div').removeClass();
+		x = e.target.id
+		$('#contentBox').removeClass();
+		$('#contentBox').addClass(x);
+	};
+
+	function dotClick(e)
+	{
+		
+		x = e.target.id
+		y = 'b' + x;
+		$('#blurbs div').removeClass();
+		$('#' + y).addClass('active');
+	};
+});
